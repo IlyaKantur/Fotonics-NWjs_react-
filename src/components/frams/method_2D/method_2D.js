49f685fe6c0d33958f23c4b85202d3d014d704b6
IlyaKantur <il.kantur@mail.ru>
2020-12-18 14:26:50 +0500

    .

--- src/components/frams/method_2D/method_2D.js
+++ src/components/frams/method_2D/method_2D.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './method_2D.css';
 
-const Method_2D = () =>{
-    return(
-        <h1>Двумерный метод</h1>
-    )
+export default class Method_2D extends Component {
+    render() {
+        return (
+            <div id="l_p_panel">
+                <div id="left_Panel" className="panel">
+
+                </div>
+                <div id="midle_Panel">
+                    <div id="up_Panel" className="panel">
+                        <div id="img">
+                            <canvas id="ImgNew"></canvas>
+                            <canvas id="ImgSum"></canvas>
+                        </div>
+                        <div id="graf">
+                            <div id="Graf">
+                                <canvas id="myChart"></canvas>
+                            </div>
+                        </div>
+                        <div id="hidden_elem">
+                            <canvas id="hiddenimg"></canvas>
+                            <canvas id="hiddenimgsum"></canvas>
+                        </div>
+                    </div>
+
+                    <div id="bottom_Panel" className="panel">
+                        <div id="panel_mid_bot">
+                            <div className="tab">Консоль</div>
+                        </div>
+                        <div id="Consol"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 }
 
-export default Method_2D;
+Method_2D;
