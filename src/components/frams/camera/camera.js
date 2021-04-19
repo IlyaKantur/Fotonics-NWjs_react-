bda01249cc8eea66ee445b5cbc8d92c839fc7dc4
Ilay Kantur <il.kantur@mail.ru>
2021-04-19 14:07:42 +0500

    .

--- src/components/frams/camera/camera.js
+++ src/components/frams/camera/camera.js
@@ -9,7 +9,23 @@ export default class Camera extends Component {
     super(props);
     this.webcamRef = React.createRef();
     this.iter;
-    this.camers = null;
+    this.n_snapshot = 0;
+    this.masInformation = {
+      countSnapshot: 1,
+      delayTime: 1
+    }
+  }
+
+  state = {
+    active: false,
+    imgSrc: null,
+    choice: false,
+    camers: [],
+    deviceId: null
+  }
+
+  stored_value = (name, value) => {
+    this.masInformation[name] = value
   }
 
   componentDidMount() {
@@ -17,14 +33,11 @@ export default class Camera extends Component {
   }
 
   handleDevices = (mediaDevices) => {
-    this.camers = mediaDevices.filter(({ kind }) => kind === "videoinput")
-    // console.log(this.camers)
-  }
-
-  state = {
-    active: false,
-    imgSrc: null,
-    choice: false
+    let camers = mediaDevices.filter(({ kind }) => kind === "videoinput")
+    console.log(camers)
+    this.setState({
+      camers: camers
+    })
   }
 
   active_cam = () => {
@@ -33,64 +46,93 @@ export default class Camera extends Component {
     })
   }
 
-  switch_choice = () =>{
+  switch_choice = () => {
     this.setState({
       choice: !this.state.choice
     })
   }
 
+  choice_camera = (item) => {
+    this.setState({
+      deviceId: item.deviceId
+    })
+    console.log(item)
+  }
+
   capture = () => {
-    let i = 0;
-    // if(!this.iter){
-    //   this.iter = setInterval(() =>{
+    this.n_snapshot = 0;
+    if (!this.iter) {
+      this.iter = setInterval(() => {
+        this.snapshot()
+      }, this.masInformation.delayTime*1000)
+    }
+  }
 
-    //   })
-    // }
-
+  snapshot = () => {
     const imageSrc = this.webcamRef.current.getScreenshot();
     this.setState({
       imgSrc: imageSrc
     })
     const base64Data = imageSrc.replace(/data:image\/png;base64,/, "");
-    fs.writeFile(`result/image/test_${i++}.jpg`, base64Data, 'base64', err => { if (err !== null) console.log(err) })
+    fs.writeFile(`result/image/test_${this.n_snapshot++}.jpg`, base64Data, 'base64', err => { if (err !== null) console.log(err) })
+    if(this.n_snapshot == this.masInformation.countSnapshot) clearInterval(this.iter)
   }
 
   render() {
-    const { active, imgSrc, choice } = this.state;
-    let element, style = '', width = 800, height = 600;
-    if(choice){
+    const { active, imgSrc, choice, camers, deviceId } = this.state;
+    let element, style = '', width = 800, height = 600, camera;
+    if (choice) {
       style = 'choice_snapshot';
       width = 400;
       height = 300
     }
     if (active) {
       element =
-      (<>
-        <div onClick = {this.switch_choice}>
-          <Webcam
-            audio={false}
-            ref={this.webcamRef}
-            screenshotFormat="image/png"
-            width={width}
-            height={height}
-          />
-        </div>
-        {imgSrc && (
-          <img id='snapshot'
-            className = {style}
-            src={imgSrc}
-            onClick = {this.switch_choice}
-          />
-        )}
-      </>
-      )
+        (<>
+          <div onClick={this.switch_choice}>
+            <Webcam
+              audio={false}
+              ref={this.webcamRef}
+              videoConstraints={deviceId}
+              screenshotFormat="image/png"
+              width={width}
+              height={height}
+            />
+          </div>
+          {imgSrc && (
+            <img id='snapshot'
+              className={style}
+              src={imgSrc}
+              onClick={this.switch_choice}
+            />
+          )}
+        </>
+        )
     }
     else { element = (<></>) }
+    if(camers.lengh != 0)
+    {
+      camera = camers.map((item, i) => {
+        return(
+          <a key={`camera_${i}`} onClick = {() => this.choice_camera(item)}>{item.label}</a>
+        )
+      })
+    }
     return (
       <div id='camera_place'>
         <div id="control">
-          <button onClick={this.active_cam}>Watch</button>
-          <button onClick={this.capture}>Capture photo</button>
+          <h3>Спосок подключенных камер</h3>
+          <div id="list_camera">
+            {camera}
+          </div>
+          <button onClick={this.active_cam}>Камера</button>
+          <button onClick={this.capture}>Накопление</button>
+          <input id='countSnapshot' type='number' placeholder="Количество снимков"
+            onChange={(e) => this.stored_value(e.target.id, e.target.value)}
+          ></input>
+          <input id='delayTime' type='number' placeholder="Время задержки,с"
+            onChange={(e) => this.stored_value(e.target.id, e.target.value)}
+          ></input>
           {/* <button onClick={stop}>Stop</button> */}
         </div>
         <div id="place">
